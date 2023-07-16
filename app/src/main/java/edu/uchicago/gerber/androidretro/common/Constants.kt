ac3db8c73369f374b611a1d9228c215c0a7d7744
tranx897 <ltran10@uchicago.edu>
2023-07-16 15:00:08 -0700

    Create Constants file

---
+++ app/src/main/java/edu/uchicago/gerber/androidretro/common/Constants.kt
@@ -0,0 +1,12 @@
+package edu.uchicago.gerber.androidretro.common
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+object Constants {
+
+    val modifier = Modifier.padding(paddingValues = PaddingValues(all = 0.dp))
+
+}
