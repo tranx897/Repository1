489d670cc0275248a049b21c70c3c6df39259615
tranx897 <ltran10@uchicago.edu>
2023-07-16 17:43:42 -0700

    implement remaining function of MovieViewModel

--- app/src/main/java/edu/uchicago/gerber/androidretro/presentation/viewmodels/MovieViewModel.kt
+++ app/src/main/java/edu/uchicago/gerber/androidretro/presentation/viewmodels/MovieViewModel.kt
@@ -3,9 +3,23 @@ package edu.uchicago.gerber.androidretro.presentation.viewmodels
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import edu.uchicago.gerber.androidretro.common.Constants
+import edu.uchicago.gerber.androidretro.data.repository.ApiProvider
+import edu.uchicago.gerber.androidretro.data.repository.MoviesRepository
+import edu.uchicago.gerber.androidretro.presentation.screens.search.paging.SearchState
+import edu.uchicago.gerber.androidretro.data.models.Result
+import edu.uchicago.gerber.androidretro.presentation.screens.search.paging.MovieSource
+import edu.uchicago.gerber.androidretro.presentation.screens.search.paging.Paginate
+import edu.uchicago.gerber.androidretro.presentation.screens.search.paging.SearchOperation
+import kotlinx.coroutines.launch
 
 class MovieViewModel : ViewModel() {
 
+    private val moviesRepository: MoviesRepository = MoviesRepository(ApiProvider.moviesApi())
 
     //////////////////////////////////////////
     // MUTABLE-STATES AND OBSERVABLE STATES
@@ -13,16 +27,42 @@ class MovieViewModel : ViewModel() {
     private var _queryText = mutableStateOf("")
     val queryText: State<String> = _queryText
 
+    private var _book = mutableStateOf(Constants.fakeMovie)
+    val book: State<Result> = _book
+
+    private val _searchState = mutableStateOf(SearchState())
+    val searchState: State<SearchState> = _searchState
+
 
     //////////////////////////////////////////
     // FUNCTIONS
     //////////////////////////////////////////
+    fun setBook(book: Result) {
+        _book.value = book
+    }
+
     fun setQueryText(query: String) {
         _queryText.value = query
     }
 
 
     fun onSearch() {
-        // TODO: search functionality to be implemented
+        _searchState.value = SearchState(searchOperation = SearchOperation.LOADING)
+        viewModelScope.launch {
+            _searchState.value = SearchState(
+                data = Pager(
+                    config = PagingConfig(pageSize = 10, prefetchDistance = 5),
+                    pagingSourceFactory = {
+                        MovieSource(
+                            booksRepository = moviesRepository,
+                            paginateData = Paginate(
+                                query = _queryText.value,
+                            )
+                        )
+                    }
+                ).flow.cachedIn(viewModelScope),
+                searchOperation = SearchOperation.DONE
+            )
+        }
     }
 }
