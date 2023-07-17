ad86e9012079eaf884ae386159b53b2cd1b01e7d
tranx897 <ltran10@uchicago.edu>
2023-07-16 18:46:07 -0700

    Create BookList and BookRow for search screen

---
+++ app/src/main/java/edu/uchicago/gerber/androidretro/presentation/screens/search/paging/MovieList.kt
@@ -0,0 +1,83 @@
+package edu.uchicago.gerber.androidretro.presentation.screens.search.paging
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.paging.LoadState
+import edu.uchicago.gerber.androidretro.presentation.navigation.Screen
+import edu.uchicago.gerber.androidretro.presentation.viewmodels.MovieViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
+
+
+@Composable
+fun MovieList(movieViewModel: MovieViewModel, navController: NavController) {
+
+    //this is what consumes the flow
+    val lazyPagingItems = movieViewModel.searchState.value.data?.collectAsLazyPagingItems()
+
+    LazyColumn {
+        items(
+            count = lazyPagingItems!!.itemCount,
+            key = lazyPagingItems.itemKey(),
+            contentType = lazyPagingItems.itemContentType()
+        ) { index ->
+            //the following lines define the onItemClick behavior
+            val boolItem = lazyPagingItems[index]!!
+            MovieRow(movie = boolItem) {
+                //the following lines define the onItemClick behavior
+                movieViewModel.setBook(boolItem)
+                navController.navigate(
+                    route = Screen.Detail.route
+                )
+            }
+
+        }
+
+        //this will display a spinner in-place of a BookRow in the following events
+        lazyPagingItems.apply {
+            //fallthrough is not supported
+            when {
+                loadState.refresh is LoadState.Loading -> {
+                    item {
+                        Spinner()
+                    }
+                }
+                loadState.append is LoadState.Loading -> {
+                    item {
+                        Spinner()
+                    }
+                }
+                loadState.prepend is LoadState.Loading -> {
+                    item {
+                        Spinner()
+                    }
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Spinner() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .padding(12.dp)
+                .align(
+                    Alignment.Center
+                )
+        )
+    }
+}
---
+++ app/src/main/java/edu/uchicago/gerber/androidretro/presentation/screens/search/paging/MovieRow.kt
@@ -0,0 +1,88 @@
+package edu.uchicago.gerber.androidretro.presentation.screens.search.paging
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.coil.rememberCoilPainter
+import edu.uchicago.gerber.androidretro.common.Constants
+import edu.uchicago.gerber.androidretro.data.models.Result
+
+@Composable
+fun MovieRow(
+    movie: Result,
+    onItemClick: (String) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                movie.id?.let {
+                    //passed down from composable (BookList) that instantiates BookRow
+                    onItemClick(it)
+                }
+            }
+            .padding(10.dp, 5.dp, 5.dp, 10.dp),
+        shape = RoundedCornerShape(corner = CornerSize(4.dp)),
+        elevation = CardDefaults.cardElevation(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    ) {
+        Row(horizontalArrangement = Arrangement.Start) {
+
+            Surface(modifier = Modifier.padding(0.dp, 0.dp, 10.dp, 0.dp)) {
+                //we use coil library here to get fadeIn effect
+                val image = rememberCoilPainter(
+                    request = movie.primaryImage?.url?.replace("http", "https")?: "https://picsum.photos/id/1026/60/90",
+                    fadeIn = true)
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(60.dp)
+                        .height(90.dp),
+                    contentScale = ContentScale.FillHeight
+                )
+
+            }
+
+            Column() {
+                Text(
+                    text = movie.titleText?.getText()?: "None",
+                    style = TextStyle(fontWeight = FontWeight.Bold),
+                    textAlign = TextAlign.Start,
+                    fontSize = 18.sp,
+                )
+                Text(text = movie.releaseYear?.getYear()?.toString()?: "None")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MovieRowPreview() {
+    MovieRow(movie = Constants.fakeMovie, onItemClick = {})
+}
