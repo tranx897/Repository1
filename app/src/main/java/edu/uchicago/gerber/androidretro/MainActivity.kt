346ae04c6ca120829c543dc4773144375805141b
tranx897 <ltran10@uchicago.edu>
2023-07-16 19:10:44 -0700

    use AnimatedNavHost to implement animation

--- app/src/main/java/edu/uchicago/gerber/androidretro/MainActivity.kt
+++ app/src/main/java/edu/uchicago/gerber/androidretro/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.uchicago.gerber.androidretro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,15 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import edu.uchicago.gerber.androidretro.presentation.navigation.Navigation
 import edu.uchicago.gerber.androidretro.ui.theme.AndroidRetroTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidRetroTheme() {
-                val navController = rememberNavController()
+                val navController = rememberAnimatedNavController()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
--- app/src/main/java/edu/uchicago/gerber/androidretro/presentation/navigation/Navigation.kt
+++ app/src/main/java/edu/uchicago/gerber/androidretro/presentation/navigation/Navigation.kt
@@ -1,23 +1,26 @@
 package edu.uchicago.gerber.androidretro.presentation.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
 import edu.uchicago.gerber.androidretro.presentation.screens.contact.ContactScreen
 import edu.uchicago.gerber.androidretro.presentation.screens.details.DetailsScreen
 import edu.uchicago.gerber.androidretro.presentation.screens.favorites.FavoritesScreen
 import edu.uchicago.gerber.androidretro.presentation.screens.search.SearchScreen
 import edu.uchicago.gerber.androidretro.presentation.viewmodels.MovieViewModel
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Navigation(
     navController: NavHostController,
     movieViewModel: MovieViewModel = viewModel()
 ) {
 
-    NavHost(navController, startDestination = Screen.Search.route) {
+    AnimatedNavHost(navController, startDestination = Screen.Search.route) {
         composable(Screen.Search.route) {
             SearchScreen(movieViewModel,navController)
 
@@ -31,7 +34,7 @@ fun Navigation(
         }
 
         composable(Screen.Detail.route) {
-            DetailsScreen()
+            DetailsScreen(movieViewModel, navController)
         }
     }
 }
