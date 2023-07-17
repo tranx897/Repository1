803f949ae35ec5a854b7ac0b118a56b58050d311
tranx897 <ltran10@uchicago.edu>
2023-07-16 18:52:29 -0700

    implement SearchScreen with internet permissions

--- app/src/main/java/edu/uchicago/gerber/androidretro/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/edu/uchicago/gerber/androidretro/presentation/screens/search/SearchScreen.kt
@@ -1,9 +1,14 @@
 package edu.uchicago.gerber.androidretro.presentation.screens.search
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,9 +23,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import edu.uchicago.gerber.androidretro.common.Constants
+import edu.uchicago.gerber.androidretro.presentation.screens.search.paging.MovieList
+import edu.uchicago.gerber.androidretro.presentation.screens.search.paging.SearchOperation
 import edu.uchicago.gerber.androidretro.presentation.viewmodels.MovieViewModel
 import edu.uchicago.gerber.androidretro.presentation.widgets.BottomNavigationBar
 import edu.uchicago.gerber.androidretro.presentation.widgets.CustomOutlinedTextField
@@ -31,6 +39,7 @@ fun SearchScreen(
     movieViewModel: MovieViewModel,
     navController: NavController,
 ) {
+    val state = movieViewModel.searchState.value
     val queryText = movieViewModel.queryText.value
 
     Scaffold(
@@ -72,6 +81,31 @@ fun SearchScreen(
                 ImeAction.Search,
                 movieViewModel::onSearch
             )
+
+            Spacer(modifier = Modifier.height(10.dp))
+            when (state.searchOperation) {
+                SearchOperation.LOADING -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .align(
+                                    Alignment.Center
+                                )
+                        )
+                    }
+                }
+                SearchOperation.DONE -> {
+                    MovieList(movieViewModel, navController)
+                }
+                else -> {
+                    Box {}
+                }
+            }
         }
     }
 }
